0a587465be3e39bfc3653f4d5cfe9f765d7f3e87
root <root@ip-172-31-27-182.us-west-1.compute.internal>
2015-11-01 05:36:11 +0000

    add file

---
+++ qa-answer-rate-widget-lang.php
@@ -0,0 +1,15 @@
+<?php
+/*
+	Question2Answer Plugin: Answer Rate Widget
+*/
+
+	return array(
+		'answer_rate_events' => '回答率及び回答平均時間',
+		'answer_rate' => '現在の回答率',
+		'average_time' => '回答までの平均時間',
+	);
+	
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
